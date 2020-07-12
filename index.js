5fde3e1df77c382403581370b2304a743851af39
scarpantonioc <scarpantonioc@gmail.com>
2020-07-12 09:49:32 -0600

    cors new implementation

--- index.js
+++ index.js
@@ -17,7 +17,23 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.json());
 
-app.use(cors());
+var allowedOrigins = ["http://localhost:1234", "*"];
+// CORS implementation
+app.use(
+  cors({
+    origin: function(origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        var message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    }
+  })
+);
 
 let auth = require("./auth")(app);
 
